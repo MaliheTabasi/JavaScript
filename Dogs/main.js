fbc1d54f4a574c6f5b23ae44a7043eeee5f56593
Malihe <maliheh73@gmail.com>
2020-10-01 18:21:43 +0330

    Dogs images slider is completed

---
+++ Dogs/main.js
@@ -0,0 +1,95 @@
+// fetch('https://cors-anywhere.herokuapp.com/https://dog.ceo/api/breeds/list/all')
+//     .then(function (response) {
+//         console.log(response);
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//     });
+let timer;
+let deleteFirstPhotoDelay;
+async function getBreeds() {
+    try {
+        const response = await fetch('https://dog.ceo/api/breeds/list/all');
+        const data = await response.json();
+        createBreedsList(data.message);
+    } catch (e) {
+        alert('there was a problem fetching breed list');
+    }
+}
+
+getBreeds();
+
+function createBreedsList(breedList) {
+    document.getElementById('breed').innerHTML = `
+    <select onchange='loadByBreed(this.value)'>
+    <option>Choose a dog breed</option>
+    ${Object.keys(breedList)
+        .map(function (breed) {
+            return `<option>${breed}</option>`;
+        })
+        .join('')}
+    </select>
+    `;
+}
+
+async function loadByBreed(breed) {
+    if (breed != 'Choose a dog breed') {
+        const response = await fetch(
+            `https://dog.ceo/api/breed/${breed}/images`
+        );
+        const data = await response.json();
+        createSlideshow(data.message);
+    }
+}
+
+function createSlideshow(images) {
+    let currentPosition = 0;
+    clearInterval(timer);
+    clearTimeout(deleteFirstPhotoDelay);
+
+    if (images.length > 1) {
+        document.getElementById('slideshow').innerHTML = `<div
+        class="slide"
+        style="
+            background-image: url(${images[0]});
+        "></div>
+    <div
+        class="slide"
+        style="
+            background-image: url(${images[1]});
+        "></div>`;
+        currentPosition += 2;
+
+        if (images.length == 2) currentPosition = 0;
+
+        timer = setInterval(nextSlide, 3000);
+    } else {
+        document.getElementById('slideshow').innerHTML = `<div
+        class="slide"
+        style="
+            background-image: url(${images[0]});
+        "></div>
+        <div class="slide"></div>`;
+    }
+
+    function nextSlide() {
+        document.getElementById('slideshow').insertAdjacentHTML(
+            'beforeend',
+            `<div
+            class="slide"
+            style="
+                background-image: url(${images[currentPosition]});
+            "></div>`
+        );
+        deleteFirstPhotoDelay = setTimeout(function () {
+            document.querySelector('.slide').remove();
+        }, 1000);
+
+        if (currentPosition + 1 >= images.length) {
+            currentPosition = 0;
+        } else {
+            currentPosition++;
+        }
+    }
+}
